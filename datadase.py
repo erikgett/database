bc96c22bd1358cb87497479a4621ab171aa46d3d
erik_ <erik_gette@mail.ru>
2022-05-04 10:56:17 +0500

    creating main function

--- datadase.py
+++ datadase.py
@@ -9,17 +9,21 @@ Maga 89995490719
 """
 
 import sqlite3
+from sqlite3 import Error
+
 
 #  СОЗДАЁМ ТАБЛИЦУ ПОЛЬЗОВАТЕЛЕЙ
 with sqlite3.connect('database.db') as db:
     cursor = db.cursor()
     query = '''
-    CREATE TABLE IF NOT EXISTS users(
+    CREATE TABLE IF NOT EXISTS usersf(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT,
         number TEXT)        
     '''
     cursor.execute(query)
+    cursor.execute("INSERT INTO usersf VALUES(1, 'John', 700 )")
+    print(155)
 
 
 #   СОЗДАЕМ ЛОГИКУ СОЗДАНИЯ - ПОИСКА id  в базе данных проверки создан ли пользователь
@@ -31,9 +35,16 @@ def User(username='erik', usernumber=891234):
 
 
 #   СОЗДАЕМ ДЛЯ ПОЛЬЗОВАТЕЛЯ ТАБЛИЦУ С ЕГО СООБЩЕНИЯМИ
-def UserMessageTable(identifier):
-    pass
+def UserMessageTable(identifier =15, sms = '234'):
+    print(identifier)
+    db = sqlite3.connect('database.db')
+    cursor = db.cursor()
+    query ='CREATE TABLE IF NOT EXISTS Users(id INTEGER PRIMARY KEY AUTOINCREMENT, name text)'
+    cursor.execute(query)
+    db.commit()
+    db.close()
 
+UserMessageTable()
 
 if __name__ == "__main__":
     import doctest
