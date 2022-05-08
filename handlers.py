5df963b67ebf7b06283945919424121090625f67
erik_ <erik_gette@mail.ru>
2022-05-08 21:02:00 +0500

    база данных есть

--- handlers.py
+++ handlers.py
@@ -1,15 +1,23 @@
 from main import bot, dp
 from aiogram.types import Message
 from config import admin_id
-from database import PushUserSmsInDb,ShowUserMessage
+from database import PushUserSmsInDb, ShowUserMessage
 
 
-async def send_to_admin(*args) :
-    await bot.send_message ( chat_id=admin_id, text="Бот запущен" )
+async def send_to_admin(*args):
+    await bot.send_message(chat_id=admin_id, text="Бот запущен")
 
 
-@dp.message_handler ( )
-async def echo(message: Message) :
+@dp.message_handler(content_types=['photo', 'text', 'video', 'audio', 'sticker'])
+async def printing(message):
+    print(message)
+    PushUserSmsInDb(message)
+    ShowUserMessage(message.from_user.id)
+    await message.reply(text=message)
+
+
+@dp.message_handler()
+async def echo(message: Message):
     text = f"привет писака: {message.text} \n"
     wat = f"письмо отправлено: {message.date} \n"
     wat2 = f"ваш id: {message.from_user.id} \n"
@@ -17,29 +25,29 @@ async def echo(message: Message) :
     wat4 = f"ваша фамилия: {message.from_user.last_name} \n"
     wat5 = f"ваш ник: {message.from_user.username} \n"
     wat6 = f"id сообщения: {message.message_id} \n"
-    PushUserSmsInDb(message)
-    ShowUserMessage(message.from_user.id)
-    await message.reply ( text=text + wat + wat2 + wat3 + wat4 + wat5 + wat6 )
+
+    await message.reply(text=text + wat + wat2 + wat3 + wat4 + wat5 + wat6)
 
 
-@dp.message_handler ( content_types=['photo', 'text'] )
-async def handle_docs_photo(message) :
+@dp.message_handler(content_types=['photo', 'text'])
+async def handle_docs_photo(message):
     photo_id = message.photo[-1].file_id
     caption = f"привет писака: {message.caption}"
-    await bot.send_photo ( message.from_user.id, photo_id )
-    await bot.send_photo ( -454011048, photo_id )
-    await message.reply ( text=caption )
-    print ( message )
+    await bot.send_photo(message.from_user.id, photo_id)
+    await bot.send_photo(-454011048, photo_id)
+    await message.reply(text=caption)
 
 
-@dp.message_handler ( content_types=['video'] )
-async def video_file_id(message) :
-    print (message)
+@dp.message_handler(content_types=['video'])
+async def video_file_id(message):
+    print()
 
-@dp.message_handler ( content_types=['audio'] )
-async def audio_file_id(message) :
-    print (message)
 
-@dp.message_handler ( content_types=['sticker'] )
-async def sticker_file_id(message) :
-    print ( message )
+@dp.message_handler(content_types=['audio'])
+async def audio_file_id(message):
+    print()
+
+
+@dp.message_handler(content_types=['sticker'])
+async def sticker_file_id(message):
+    print()
